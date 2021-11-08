a600e8187ae93098cbd93086248ff2bf5d31bbd0
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-08 09:08:07 +0300

    config.py file

--- config.py
+++ config.py
@@ -15,3 +15,14 @@ class Config:
     # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # TESTING = True
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
