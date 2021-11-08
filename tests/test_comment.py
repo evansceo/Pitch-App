a96b79ac619e8a733e3873f187fd2c5c1566cf0f
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-08 09:13:43 +0300

    testing comment

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -8,3 +8,7 @@ class CommentModelTest(unittest.TestCase):
         self.new_pitch = Pitch(id=1,pitch_title='Test',pitch_content='This is a test pitch',category="interview",user = self.user_Peris,likes=0,dislikes=0)
         self.new_comment = Comment(id=1,comment='Test comment',user=self.user_Peris,pitch=self.new_pitch)
 
+    def tearDown(self):
+        Pitch.query.delete()
+        User.query.delete()
+
