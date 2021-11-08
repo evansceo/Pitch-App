bf53cf8902465c21b465acc9e7ad3753accdf47e
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-08 09:01:37 +0300

    manage.py file

--- manage.py
+++ manage.py
@@ -3,4 +3,10 @@ from flask_script import Manager,Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
+#Creating app instance
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
 
